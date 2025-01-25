dc5f3fef252e2d9540eb9e076ea4dff8705ac6a8
AskatAsh <abdulaskat@gmail.com>
2025-01-25 15:03:22 +0600

    started project timely with vite react tailwindcss and added favicon with assets and installed some packages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-4xl font-bold'>Timely | Home</h1>
+      <p>A delivery platform that delivers parcels on time.</p>
+      
+    </>
+  )
+}
+
+export default App
