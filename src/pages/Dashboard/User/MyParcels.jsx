54185dc9fc8531c20c6a9f4c8c69a72a19a391a3
AskatAsh <abdulaskat@gmail.com>
2025-01-28 02:38:12 +0600

    added modal with from using shadcn ui to add review using post api

--- src/pages/Dashboard/User/MyParcels.jsx
+++ src/pages/Dashboard/User/MyParcels.jsx
@@ -9,12 +9,61 @@ import {
   TableHeader,
 } from "@/components/ui/table";
 import { Link } from "react-router-dom";
+import { useCallback, useState } from "react";
+import useAuth from "./../../../hooks/useAuth";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { FaStar } from "react-icons/fa";
+import useAxiosSecure from "./../../../hooks/useAxiosSecure";
+import { enqueueSnackbar, SnackbarProvider } from "notistack";
 
 const MyParcels = () => {
+  const { user } = useAuth();
   const [myParcels] = useGetMyParcels();
+  const [isOpen, setIsOpen] = useState(false);
+  const [parcel, setParcel] = useState("");
+  const axiosSecure = useAxiosSecure();
+
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+    setParcel("");
+  }, []);
+
+  const handleAddReview = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userName = form.username.value;
+    const userImage = form.userImage.value;
+    const deliverymanId = form.deliverymanId.value;
+    const rating = form.rating.value;
+    const review = form.review.value;
+    const reviewInfo = {
+      userName,
+      userImage,
+      deliverymanId,
+      rating,
+      review,
+    };
+    // console.log(reviewInfo);
+    const res = await axiosSecure.post("/addReview", reviewInfo);
+    console.log(res);
+    if (res.data.insertedId) {
+      enqueueSnackbar("Booked Delivery Successfully!", { variant: "success" });
+      setIsOpen(false);
+      setParcel("");
+    }
+  };
 
   return (
-    <div className="p-4">
+    <section className="p-4">
+      <SnackbarProvider
+        autoHideDuration={2000}
+        anchorOrigin={{
+          vertical: "center",
+          horizontal: "center",
+        }}
+      />
       <h1 className="text-2xl font-bold mb-4">My Parcels</h1>
       <p className="mb-4">My total parcels: {myParcels.length}</p>
       <div className="overflow-x-auto">
@@ -40,14 +89,38 @@ const MyParcels = () => {
                 <TableCell>{parcel?.deliverymanId || "N/A"}</TableCell>
                 <TableCell>
                   {parcel?.status === "delivered" ? (
-                    <Button size="xs" className="bg-green-600">Add Review</Button>
-                  ): `${parcel?.status}`}
+                    <Button
+                      onClick={() => {
+                        setIsOpen(true);
+                        setParcel(parcel);
+                      }}
+                      size="xs"
+                      className="bg-green-600 "
+                    >
+                      Add Review
+                    </Button>
+                  ) : (
+                    `${parcel?.status}`
+                  )}
                 </TableCell>
                 <TableCell className="flex flex-col gap-2">
-                  <Button size="xs" className="bg-primary text-xs" disabled={parcel?.status !== "pending"}>
-                    <Link to={`/dashboard/updateParcel/${parcel._id}`} state={{parcel}}>Update</Link>
+                  <Button
+                    size="xs"
+                    className="bg-primary text-xs"
+                    disabled={parcel?.status !== "pending"}
+                  >
+                    <Link
+                      to={`/dashboard/updateParcel/${parcel._id}`}
+                      state={{ parcel }}
+                    >
+                      Update
+                    </Link>
                   </Button>
-                  <Button size="xs" className="bg-red-600" disabled={parcel?.status !== "pending"}>
+                  <Button
+                    size="xs"
+                    className="bg-red-600"
+                    disabled={parcel?.status !== "pending"}
+                  >
                     Cancel
                   </Button>
                 </TableCell>
@@ -56,7 +129,85 @@ const MyParcels = () => {
           </TableBody>
         </Table>
       </div>
-    </div>
+
+      {/* Modal to assign deliveryman */}
+      {isOpen && (
+        <div className="w-full items-center justify-center bg-[#00000050] backdrop-blur-sm absolute top-0 left-0 min-h-screen flex">
+          <form
+            onSubmit={handleAddReview}
+            className="max-w-lg w-full p-5 rounded-xl shadow-lg m-4 bg-background border border-secondary-800 space-y-3"
+          >
+            <div className="flex items-center justify-center gap-2 mb-4">
+              <FaStar className="text-yellow-500" />{" "}
+              <h3 className="text-lg font-bold">Give a Review</h3>
+            </div>
+            {/* username */}
+            <div className="space-y-2">
+              <Label htmlFor="username">User Name</Label>
+              <Input
+                id="username"
+                type="text"
+                defaultValue={user?.displayName}
+                readOnly
+              />
+            </div>
+
+            {/* photo url */}
+            <div className="space-y-2">
+              <Label htmlFor="userImage">User PhotoURL</Label>
+              <Input
+                id="userImage"
+                type="url"
+                defaultValue={user?.photoURL}
+                readOnly
+              />
+            </div>
+
+            {/* delveryman's Id */}
+            <div className="space-y-2">
+              <Label htmlFor="deliverymanId">Deliveryman Id</Label>
+              <Input
+                id="deliverymanId"
+                type="text"
+                defaultValue={parcel?.deliverymanId}
+                readOnly
+              />
+            </div>
+
+            {/* Review */}
+            <div className="space-y-2">
+              <Label htmlFor="rating">Rating (out of 5)</Label>
+              <Input
+                id="rating"
+                type="number"
+                name="rating"
+                min="1"
+                max="5"
+                step={0.1}
+              />
+            </div>
+
+            {/* Review */}
+            <div className="space-y-2">
+              <Label htmlFor="review">Review comment</Label>
+              <Textarea id="review" type="number" name="review" />
+            </div>
+
+            <div className="flex items-center justify-center gap-4 mt-3">
+              <Button type="submit" className="bg-green-600/60 text-text">
+                Add Review
+              </Button>
+            </div>
+          </form>
+          <Button
+            onClick={closeModal}
+            className="bg-red-500 text-white absolute top-5 right-5"
+          >
+            X
+          </Button>
+        </div>
+      )}
+    </section>
   );
 };
 
