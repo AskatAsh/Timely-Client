6f78a9b426c41f1603dd1d70e1c5d7c633ee9c58
AskatAsh <abdulaskat@gmail.com>
2025-01-28 17:03:41 +0600

    added stats like totalusers, totaldeliveris, totalparcel data in admin dashboard

--- src/pages/Dashboard/Admin/AdminDashboard.jsx
+++ src/pages/Dashboard/Admin/AdminDashboard.jsx
@@ -1,10 +1,40 @@
+import useGetStats from "./../../../hooks/useGetStats";
 
 const AdminDashboard = () => {
-    return (
-        <div>
-            Admin Dashboard.
+  const [stats] = useGetStats();
+
+  return (
+    <div>
+      <section className="bg-primary-100 p-6 rounded-3xl">
+        <h2 className="text-3xl font-extrabold text-text mb-6 my-2 text-center">
+          Statistics
+        </h2>
+        <div className="max-w-7xl w-full mx-auto flex justify-center gap-8 flex-wrap mt-10">
+          {/* stat 1 */}
+          <div className="bg-primary-100 p-6 rounded-2xl text-center flex-1">
+            <h3 className="font-bold text-primary-600">Percel Booked</h3>
+            <p className="text-6xl font-extrabold text-primary pt-3">
+              {stats.totalParcelsBooked}
+            </p>
+          </div>
+          {/* stat 2 */}
+          <div className="bg-primary-100 p-6 rounded-2xl text-center flex-1">
+            <h3 className="font-bold text-primary-600">Percels Delivered</h3>
+            <p className="text-6xl font-extrabold text-primary pt-3">
+              {stats.totalDelivered}
+            </p>
+          </div>
+          {/* stat 3 */}
+          <div className="bg-primary-100 p-6 rounded-2xl text-center flex-1">
+            <h3 className="font-bold text-primary-600">Total Users</h3>
+            <p className="text-6xl font-extrabold text-primary pt-3">
+              {stats.totalUsers}
+            </p>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default AdminDashboard;
